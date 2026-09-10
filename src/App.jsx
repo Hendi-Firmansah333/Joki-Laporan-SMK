ffaa0d8283a1fe6842885d3c6ef9242c064526e6
Hendi Firmansah <hendifirmansah333@gmail.com>
2026-09-10 16:39:32 +0700

    feat: add framer-motion page transitions and smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import Home from './pages/Home';
 import ServiceEducation from './pages/ServiceEducation';
 import PaymentMethods from './pages/PaymentMethods';
 import InfoPage from './pages/InfoPage';
+import { PageTransition } from './components/PageTransition';
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
+  
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageTransition><Home /></PageTransition>} />
+        <Route path="/edukasi/:serviceId" element={<PageTransition><ServiceEducation /></PageTransition>} />
+        <Route path="/bantuan/metode-pembayaran" element={<PageTransition><PaymentMethods /></PageTransition>} />
+        <Route path="/info/:pageId" element={<PageTransition><InfoPage /></PageTransition>} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/edukasi/:serviceId" element={<ServiceEducation />} />
-        <Route path="/bantuan/metode-pembayaran" element={<PaymentMethods />} />
-        <Route path="/info/:pageId" element={<InfoPage />} />
-      </Routes>
+      <AnimatedRoutes />
     </Router>
   );
 }
